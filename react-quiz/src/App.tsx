69c1bad99a69a4eb18c804a12c3c60c4fb91482f
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-31 18:18:09 +0530

    feat: useReducer practiced and project structure defined

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,10 +1,16 @@
-import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <>
-      <DateCounter />
-    </>
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
   );
 }
 
--- react-quiz/src/components/DateCounter.tsx
+++ react-quiz/src/components/DateCounter.tsx
@@ -1,34 +1,71 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+//reducer function is the pure function that takes prev state and action as input and returns the new updated state
+
+//action is an object that has information for reducer which states how to update the state
+
+// dispatch function contains the action object that basically triggers the reducer function that upadtes the state
+
+// initial state also needs to pass to useReducer
+
+function reducer(state, action) {
+  // if (action.type === "inc") {
+  //   return state + 1;
+  // }
+  // if (action.type === "dec") {
+  //   return state - 1;
+  // }
+  // if (action.type === "setCount") {
+  //   return action.payload;
+  // }
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+
+    case "inc":
+      return { ...state, count: state.count + state.step };
+
+    case "setCount":
+      return { ...state, count: action.payload };
+
+    case "setStep":
+      return { ...state, step: action.payload };
+
+    case "reset":
+      return { count: 0, step: 0 };
+
+    default:
+      throw new Error("Unkown action");
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e: React.ChangeEvent<HTMLInputElement>) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e: React.ChangeEvent<HTMLInputElement>) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
